6810f4255002a6e27e7d0036b791e711182c26d9
PaponJob <132637951+PaponJob@users.noreply.github.com>
2023-05-26 04:55:13 +0000

    work is done

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char* argv[]) {
   student a[10]; //total = 12 student
   //s1.name="PUN";  // ERROR, it is in private
    s1.set_name("Pun"); //make a default value to your name 	
-  s1.set_age(2); 
+  s1.set_age(12); 
 		s2.set_name("Joe");
-    s2.set_age(17);
+    s2.set_age(15);
 
    s1.get_age()>s2.get_age() ?
     s1.print_name():
